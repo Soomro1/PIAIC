39d07e73d98ea1c50050e0e382f3bc17dab00187
Hassan Sikandar <86551367+Soomro1@users.noreply.github.com>
2022-10-12 00:15:42 +0500

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+
+   /* int age = 10;
+    if(age>40){
+        printf("You are old");
+        printf("\n");
+    }
+    else if(age<=19){
+printf("you are young");
+printf("\n");
+    }
+    else if(age<=10){
+        printf("\n you are child");
+    }
+
+else{
+    printf("you are new born baby");
+    printf("\n");
+}
+return 0;
+*/
+int num1 = 25;
+int num2 = 5;
+int div = num1/ num2;
+int rem = num1% num2;
+
+
+printf("\n %d dividied by %d is %d\n", num1,num2,div);
+printf("\n %d dividied by %d reminder is %d\n", num1,num2,rem);
+
+printf("\n Value of num1 Before %d\n",num1);
+    num1 = num1 + 1;
+printf("\n Value of num1 after %d\n",num1);
+
+}
